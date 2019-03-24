340791d8effe082e54537f03383f9b2631d76a96
김명준 <myungjun10@naver.com>
2019-03-25 00:48:51 +0900

    부모 오브젝트에서 자식 오브젝트 각도 돌리는 기능 추가 스테이지5 레벨 배치 완료

---
+++ Design/DesignLevelTool/Design_MaterialChange.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Design_MaterialChange : MonoBehaviour
+{
+    public Material ChangeMaterial;
+    private Material BeforeMaterial;
+
+    public bool RotPlus, RotMinus;
+    private bool ChangeRot;
+
+    void Update()
+    {
+        ChangeMaterialFunc();
+        ChangeRot3D();
+    }
+
+    void ChangeMaterialFunc()
+    {
+        if (BeforeMaterial != ChangeMaterial)
+        {
+            transform.Find("Root3D").GetComponent<MeshRenderer>().material = ChangeMaterial;
+            BeforeMaterial = ChangeMaterial;
+        }
+    }
+
+    void ChangeRot3D()
+    {
+        if (RotMinus || RotPlus)
+            ChangeRot = true;
+
+        if (ChangeRot)
+        {
+            float RotValue = 0;
+
+            if (RotPlus)
+                RotValue = 90;
+            else if (RotMinus)
+                RotValue = -90;
+
+            transform.Find("Root3D").transform.Rotate(new Vector3(0, RotValue, 0));
+
+            ChangeRot = false;
+            RotMinus = false;
+            RotPlus = false;
+        }
+    }
+}
