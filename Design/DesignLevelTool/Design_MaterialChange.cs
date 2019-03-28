65b42ba88089d4da4654adae1514fbbc0d9b756f
김헌주 <dkshqpfm@naver.com>
2019-03-28 23:45:32 +0900

    타일메터리얼추가 스카이박스 각도 수정 materalchange 수정

--- Design/DesignLevelTool/Design_MaterialChange.cs
+++ Design/DesignLevelTool/Design_MaterialChange.cs
@@ -8,14 +8,28 @@ public class Design_MaterialChange : MonoBehaviour
     public Material ChangeMaterial;
     private Material BeforeMaterial;
 
-    public bool RotPlus, RotMinus;
-    private bool ChangeRot;
+    public bool RotPlus = false;
+    public bool RotMinus = false;
+    public bool RotZero = false;
+    private bool ChangeRot = false;
+
+    void Start()
+    {
+        RotPlus = false;
+        RotMinus = false;
+        RotZero = false;
+        ChangeRot = false;
+    }
+
 
     void Update()
     {
         ChangeMaterialFunc();
         ChangeRot3D();
+        ChangeRot3DZero();
+        
     }
+
 
     void ChangeMaterialFunc()
     {
@@ -25,6 +39,7 @@ public class Design_MaterialChange : MonoBehaviour
             BeforeMaterial = ChangeMaterial;
         }
     }
+
 
     void ChangeRot3D()
     {
@@ -41,10 +56,23 @@ public class Design_MaterialChange : MonoBehaviour
                 RotValue = -90;
 
             transform.Find("Root3D").transform.Rotate(new Vector3(0, RotValue, 0));
-
+            
             ChangeRot = false;
             RotMinus = false;
             RotPlus = false;
         }
     }
+
+
+    void ChangeRot3DZero()
+    {
+
+        if (RotZero)
+        {
+
+            transform.Find("Root3D").transform.rotation = Quaternion.Euler (0, 0, 0);
+            RotZero = false;
+        }
+    }
+
 }
