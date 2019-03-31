c905e60048e1a5c79d30d7e63700ee855c111b88
김명준 <myungjun10@naver.com>
2019-03-31 22:16:53 +0900

    무빙액터 프로토타이핑 타임라인 테스트 진행

---
+++ Design/DesignScript/Design_MoveSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_MoveSwitch : MonoBehaviour
+{
+    bool SwitchBool = false;
+
+    public GameObject MovingActor;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10 && MovingActor && !SwitchBool)
+        {
+            MovingActor.GetComponent<Design_MovingActor>().IsEnabled = true;
+            SwitchBool = true;
+        }
+    }
+}
--- Design/DesignScript/Design_MovingActor.cs
+++ Design/DesignScript/Design_MovingActor.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MovingType { Once, Repeat }
+
 public class Design_MovingActor : MonoBehaviour
 {
     List<Vector3> MovePosArray = new List<Vector3>();
 
     int TargetNum;
+    int MoveDir;
+    bool bWait;
+
+    [HideInInspector]
+    public bool IsEnabled;
+
+    public bool SetEnabled;
+    public float MoveSpeed;
+    public float WaitTime;
+    public MovingType CurMoveType;
+
+
+
+
+    //InitializeValue
+
+
 
     void Start()
     {
+        InitializeValue();
         InitializeMovePos();
     }
 
@@ -19,15 +39,12 @@ public class Design_MovingActor : MonoBehaviour
     }
 
 
+
+
     //EventFunction
 
 
 
-    void ProcessMoveActor()
-    {
-        CheckTargetNum();
-        OneMove();
-    }
 
     void InitializeMovePos()
     {
@@ -36,28 +53,106 @@ public class Design_MovingActor : MonoBehaviour
             if (transform.Find("Pos (" + i + ")") != null)
             {
                 MovePosArray.Add(transform.Find("Pos (" + i + ")").gameObject.transform.position);
-                Debug.Log(MovePosArray[i]);
             }
         }
     }
 
-    void CheckTargetNum()
+    void InitializeValue()
     {
-        foreach (var v in MovePosArray)
+        TargetNum = 0;
+        MoveDir = 1;
+        bWait = false;
+        IsEnabled = SetEnabled;
+        MoveSpeed = MoveSpeed * 0.1f;
+    }
+
+
+
+
+
+
+
+    //ProcessMoveActor
+
+
+    void ProcessMoveActor()
+    {
+        if (IsEnabled)
         {
-            if (v == transform.position)
+            MoveActor();
+            SetTargetNum(CurMoveType);
+        }
+    }
+
+    void SetTargetNum(MovingType MoveType)
+    {
+        if (!bWait)
+        {
+            foreach (var v in MovePosArray)
             {
-                TargetNum++;
-                if (TargetNum > MovePosArray.Count-1)
-                    TargetNum = 0;
-                break;
+                if (v == transform.position)
+                {
+                    if (MoveType == MovingType.Once)
+                        StartCoroutine("OnceNum");
+                    else if (MoveType == MovingType.Repeat)
+                        StartCoroutine("RepeatNum");
+
+                    bWait = true;
+                    break;
+                }
             }
         }
     }
 
-    void OneMove()
+    void MoveActor()
     {
-        transform.position = transform.position + Vector3.up;
-        //transform.position = Vector3.MoveTowards(transform.position, MovePosArray[TargetNum], 0.2f);
+        if (!bWait)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MovePosArray[TargetNum], MoveSpeed);
+        }
+    }
+
+
+
+
+
+
+
+    //SetNum
+
+    void SetNum()
+    {
+        bWait = false;
+        TargetNum+= MoveDir;
+    }
+
+    void ChangeMoveDir()
+    {
+        if (TargetNum > MovePosArray.Count - 1)
+        {
+            MoveDir = -1;
+            TargetNum -= 2;
+        }
+        else if (TargetNum < 0)
+        {
+            MoveDir = 1;
+            TargetNum += 2;
+        }
+    }
+
+    IEnumerator RepeatNum()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        SetNum();
+        ChangeMoveDir();
+    }
+
+    IEnumerator OnceNum()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        SetNum();
+        IsEnabled = false;
+
+        ChangeMoveDir();
     }
 }
