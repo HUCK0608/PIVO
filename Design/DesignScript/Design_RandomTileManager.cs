0dc399aa8dd646d48cb2437617ac3d0bfcbf9173
김명준 <myungjun10@naver.com>
2019-05-30 22:51:11 +0900

    스테이지 클리어 타임라인(좌상단 방향) 완성/프리팹 생성 월드오브젝트 쉐이더 안쓰는 부분들 삭제

--- Design/DesignScript/Design_RandomTileManager.cs
+++ Design/DesignScript/Design_RandomTileManager.cs
@@ -9,6 +9,9 @@ public class Design_RandomTileManager : MonoBehaviour
     float PosX = -999f;
     float TargetPosY;
     float MoveSpeed, RandomRange, WaitTileTime;
+
+    bool bUseTileSelect = false;
+    float WaitTileSelect;
     
 
     void Start()
@@ -16,17 +19,30 @@ public class Design_RandomTileManager : MonoBehaviour
         MoveSpeed = 0.8f;
         RandomRange = 5f;
         WaitTileTime = 0.15f;
-        
+
         TargetPosY = transform.position.y;
         for (int i = 0; i < transform.childCount; i++)
         {
             TileGroup.Add(transform.GetChild(i).gameObject);
         }
-        StartCoroutine("TileSelect");
+        //StartCoroutine("TileSelect");
+        bUseTileSelect = true;
     }
 
     void Update()
     {
+        if (bUseTileSelect)
+        {
+            if (WaitTileSelect > WaitTileTime)
+            {
+                TileSelectTick();
+                WaitTileSelect = 0;
+            }
+            else
+                WaitTileSelect += Time.deltaTime;
+        }
+
+
         if (MoveTileGroup.Count > 1)
         {
             foreach (var Value in MoveTileGroup)
@@ -39,6 +55,60 @@ public class Design_RandomTileManager : MonoBehaviour
                 }                    
             }
         }
+    }
+
+    void TileSelectTick()
+    {
+        bool AllClear = false;
+        int AllValueCheck = 0;
+        foreach (var Value in TileGroup)
+        {
+            if (PosX == -999)
+            {
+                PosX = Value.transform.position.x;
+            }
+
+            bool NotArray = true;
+            foreach (var V in MoveTileGroup)
+            {
+                if (Value == V)
+                    NotArray = false;
+            }
+
+            if (NotArray)
+            {
+                if (PosX > Value.transform.position.x && Value.transform.position.y != TargetPosY)
+                    PosX = Value.transform.position.x;
+            }
+
+
+            if (Value.transform.position.y == TargetPosY)
+                AllValueCheck++;
+
+            if (AllValueCheck == TileGroup.Count)
+                AllClear = true;
+        }
+        if (!AllClear)
+        {
+            foreach (var Value in TileGroup)
+            {
+                if (PosX == Value.transform.position.x)
+                {
+                    MoveTileGroup.Add(Value);
+
+                    Vector3 ValuePos = Value.transform.position;
+                    float RandomYValue = ValuePos.y + Random.Range(-RandomRange, RandomRange);
+                    Vector3 RandomPos = new Vector3(ValuePos.x, RandomYValue, ValuePos.z);
+                    Value.transform.position = RandomPos;
+                }
+            }
+        }
+        else
+        {
+            bUseTileSelect = false;
+        }
+
+        PosX += 2;
     }
 
     IEnumerator TileSelect()
@@ -74,7 +144,6 @@ public class Design_RandomTileManager : MonoBehaviour
                 if (AllValueCheck == TileGroup.Count)
                     AllClear = true;
             }
-
             if (!AllClear)
             {
                 foreach (var Value in TileGroup)
@@ -91,8 +160,10 @@ public class Design_RandomTileManager : MonoBehaviour
                 }
             }
             else
+            {
                 break;
-
+            }
+            
             PosX += 2;
             yield return new WaitForSeconds(WaitTileTime);
         }
