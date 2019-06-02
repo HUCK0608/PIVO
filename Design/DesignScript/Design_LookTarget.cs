e7407ed8aafbdda5fc6524a36fa06e4f95312c79
김명준 <myungjun10@naver.com>
2019-06-02 22:13:01 +0900

    우하단 스테이지 클리어 타임라인 완료 큐브친구들 프로토타이핑 진행중

---
+++ Design/DesignScript/Design_LookTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_LookTarget : MonoBehaviour
+{
+    public GameObject TargetObject;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(TargetObject.transform);
+    }
+}
