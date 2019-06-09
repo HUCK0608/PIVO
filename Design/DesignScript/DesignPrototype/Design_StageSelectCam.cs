71f2e5d8e54c78e6d96c5aa1553b96f2b8b40f80
김명준 <myungjun10@naver.com>
2019-06-09 23:25:33 +0900

    1. 스테이지 선택씬 프로토타이핑 작업 시작 2. 쉐이더 정리 및 추가

---
+++ Design/DesignScript/DesignPrototype/Design_StageSelectCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_StageSelectCam : MonoBehaviour
+{
+    public GameObject TargetActor;
+    bool bUseFollowTarget;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (bUseFollowTarget)
+            FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        transform.position = TargetActor.transform.position;
+    }
+}
---
+++ Design/DesignScript/DesignPrototype/Design_StageSelectPC.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_StageSelectPC : MonoBehaviour
+{
+    List<GameObject> MovePosArray = new List<GameObject>();
+    int CurMovePos = 0;
+    int MaxMovePos = 3;
+    void Start()
+    {
+        InitializeMovePos();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (CurMovePos < MaxMovePos)
+                CurMovePos++;
+            MoveChar(MovePosArray[CurMovePos]);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (CurMovePos > 0)
+                CurMovePos--;
+            MoveChar(MovePosArray[CurMovePos]);
+        }
+    }
+
+    void InitializeMovePos()
+    {
+        for (int i = 0; i < MaxMovePos+1; i++)
+        {
+            MovePosArray.Add(GameObject.Find("MovePos (" + i + ")"));
+        }
+
+        Vector3 InitPos = MovePosArray[0].transform.position;
+        transform.position = new Vector3(InitPos.x, transform.position.y, InitPos.z);
+    }
+
+    void MoveChar(GameObject PosActor)
+    {
+        Vector3 InitPos = PosActor.transform.position;
+        transform.position = new Vector3(InitPos.x, transform.position.y, InitPos.z);
+    }
+
+    //코루틴으로 다시 만들어야 할 듯
+}
