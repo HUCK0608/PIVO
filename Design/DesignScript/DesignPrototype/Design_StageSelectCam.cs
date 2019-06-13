8e1197affb469168b2ed61effcd6e83b5a4bed1e
김명준 <myungjun10@naver.com>
2019-06-13 23:35:30 +0900

    큐브친구들 프로토타이핑

--- Design/DesignScript/DesignPrototype/Design_StageSelectCam.cs
+++ Design/DesignScript/DesignPrototype/Design_StageSelectCam.cs
@@ -7,18 +7,13 @@ public class Design_StageSelectCam : MonoBehaviour
     public GameObject TargetActor;
     public GameObject FollowCamActor;
     public GameObject DialogueCamActor;
-    Transform DialogueCamTrnasform;
-    Transform FollowCamTransform;
-    float MoveSpeed;
+    public GameObject DialogueUI;
     bool bUseFollowTarget;
     void Start()
     {
         bUseFollowTarget = true;
         DialogueCamActor.SetActive(false);
         FollowCamActor.SetActive(true);
-        FollowCamTransform = FollowCamActor.transform;
-        DialogueCamTrnasform = DialogueCamActor.transform;
-        MoveSpeed = 0.3f;
     }
 
     void Update()
@@ -33,20 +28,15 @@ public class Design_StageSelectCam : MonoBehaviour
     {
         DialogueCamActor.SetActive(false);
         FollowCamActor.SetActive(true);
-
-        if (transform.position != FollowCamTransform.position)
-            transform.position = Vector3.MoveTowards(transform.position, FollowCamTransform.position, MoveSpeed);
-        else
-            transform.position = TargetActor.transform.position;
+        DialogueUI.SetActive(false);
+        transform.position = TargetActor.transform.position;
     }
 
     void DialogueCam()
     {
         DialogueCamActor.SetActive(true);
         FollowCamActor.SetActive(false);
-
-        if (transform.position != DialogueCamTrnasform.position)
-            transform.position = Vector3.MoveTowards(transform.position, DialogueCamTrnasform.position, MoveSpeed);
+        DialogueUI.SetActive(true);
     }
 
     public void UseDialogueCamera()
