d5d8fd533bdecb6e824cc8fafd571b837fb72eb6
김명준 <myungjun10@naver.com>
2019-07-14 23:32:23 +0900

    스테이지 폴리싱작업 진행 문/열쇠 제대로 작동할 수 있도록 수정 타임라인 카메라 글로벌포그 추가

--- Design/DesignScript/DesignPrototype/Design_DoorManager.cs
+++ Design/DesignScript/DesignPrototype/Design_DoorManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Design_DoorManager : MonoBehaviour
 {
+    public CWorldManager WorldManager;
     public GameObject DoorInDoor, DoorInDoor2, KeyPrefab;
     public float AttachKeySpeed;
 
@@ -11,8 +12,10 @@ public class Design_DoorManager : MonoBehaviour
     public GameObject[] KeyObject = new GameObject[] { };
     
     private GameObject[] KeyPosArray = new GameObject[4];
+    private GameObject Door2D, Door3D;
     private int KeyNum;
     private bool IsOpen;
+    private bool bState3D, bState2D, OutViewRect;
 
 
     //InitializeValue
@@ -29,11 +32,17 @@ public class Design_DoorManager : MonoBehaviour
         SetKeyPos();
         SetKeyScript();
         SetDoorState();
+        bState3D = true;
+        bState2D = false;
+        OutViewRect = true;
+        Door2D = transform.Find("Root2D").gameObject;
+        Door3D = transform.Find("Root3D").gameObject;
     }
 
     private void Update()
     {
         OpenDoorAnim();
+        SetWorld();
     }
 
 
@@ -44,7 +53,40 @@ public class Design_DoorManager : MonoBehaviour
 
 
 
+    void SetWorld()
+    {
+        if (WorldManager)
+        {
+            if (WorldManager.CurrentWorldState == EWorldState.View2D)
+            {
+                if (bState3D)//2D로 바꾸기
+                {
+                    //Set3DRender(false);
+                    bState2D = true;
+                    bState3D = false;
 
+                    if (OutViewRect)
+                    {
+                        Set3DRender(false);
+                        Door2D.SetActive(false);
+                    }
+                    else
+                        Door2D.SetActive(true);
+                }
+            }
+            else
+            {
+                if (bState2D)//3D로 바꾸기
+                {
+                    Set3DRender(true);
+                    Door2D.SetActive(false);
+                    bState3D = true;
+                    bState2D = false;
+                    OutViewRect = true;
+                }
+            }
+        }
+    }
 
     void SetKeyPos()
     {
@@ -110,6 +152,76 @@ public class Design_DoorManager : MonoBehaviour
     public void AddKeyNum()
     {
         KeyNum++;
+        SetDoorPattern();
         SetDoorState();
+    }
+    void Set3DRender(bool bState)
+    {
+        Door3D.SetActive(bState);
+        foreach (var v in KeyPosArray)
+        {
+            if (v.transform.GetChild(0).gameObject)
+                v.transform.GetChild(0).gameObject.SetActive(bState);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OutViewRect = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OutViewRect = true;
+        }
+    }
+
+    void SetDoorPattern()
+    {
+        StartCoroutine("AddPattern");
+    }
+
+    IEnumerator AddPattern()
+    {
+        float CurPatternFill = 0;
+        float TargetFill = 0;
+
+        if (KeyNum == 1)
+        {
+            CurPatternFill = 0;
+            TargetFill = 1;
+        }
+        else if (KeyNum == 2)
+        {
+            CurPatternFill = 1;
+            TargetFill = 1.6f;
+        }
+        else if (KeyNum == 3)
+        {
+            CurPatternFill = 1.6f;
+            TargetFill = 2f;
+        }
+        else if (KeyNum == 4)
+        {
+            CurPatternFill = 2f;
+            TargetFill = 6f;
+        }
+
+        GameObject PatternObject = Door3D.transform.Find("Activate_Door").Find("Activate_Door_Pattern").gameObject;
+        Material PatternMat = PatternObject.GetComponent<Renderer>().material;
+
+        while (true)
+        {
+            CurPatternFill += 0.008f;
+            PatternMat.SetFloat("_PatternFill", CurPatternFill);
+
+            if (CurPatternFill >= TargetFill)
+                break;
+
+            yield return null;
+        }
     }
 }
