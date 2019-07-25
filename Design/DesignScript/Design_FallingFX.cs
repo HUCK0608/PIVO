1e6d2fc73ea133be03020d65c888483a41baff8b
김명준 <myungjun10@naver.com>
2019-07-25 22:41:36 +0900

    눈/꽃 내리는거 완료 설산 스테이지1 레벨수정 봄에서 겨울로 연출에 사용될 씬 추가

---
+++ Design/DesignScript/Design_FallingFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FXType { Leaf, Snow }
+public class Design_FallingFX : MonoBehaviour
+{
+    public FXType SelectFX;
+    GameObject FXSnow, FXLeaf;
+    void Start()
+    {
+        FXSnow = transform.Find("FX_Snow").gameObject;
+        FXLeaf = transform.Find("FX_Leaf").gameObject;
+
+        if (SelectFX == FXType.Leaf)
+        {
+            FXSnow.SetActive(false);
+            FXLeaf.SetActive(true);
+        }
+        else
+        {
+            FXSnow.SetActive(true);
+            FXLeaf.SetActive(false);
+        }
+    }
+}
