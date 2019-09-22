dcf573df82f54ed10fde8c58a048def52cb47888
김명준 <myungjun10@naver.com>
2019-09-22 23:25:56 +0900

    폭탄 프로토타입 진행중

---
+++ Design/DesignScript/DesignPrototype/Design_BombSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_BombSpawn : MonoBehaviour
+{
+    public GameObject Bomb;
+
+    GameObject CurBomb;
+    void Start()
+    {
+        SpawnBomb();
+    }
+
+    public void SpawnBomb()
+    {
+
+        CurBomb = Instantiate(Bomb, transform);
+        CurBomb.transform.Find("Root3D").GetComponent<Design_Bomb>().ParentBombSpawn = this;
+        StartCoroutine("RiseBomb");
+
+    }
+
+    IEnumerator RiseBomb()
+    {
+        CurBomb.transform.position = transform.position;
+        float TargetPositionY = transform.position.y + 2;
+
+        while (CurBomb.transform.position.y < TargetPositionY)
+        {
+            CurBomb.transform.position = CurBomb.transform.position + new Vector3(0, 0.1f, 0);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+    }
+}
---
+++ Design/DesignScript/DesignPrototype/Design_WorldController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Design_WorldController : MonoBehaviour
+{
+    private CWorldManager WorldManager;
+    private GameObject Actor3D, Actor2D;
+
+    private EWorldState BeforeState = EWorldState.View3D;
+
+    private bool bState3D, bState2D, OutViewRect, IsChanging;
+
+    void Start()
+    {
+        WorldManager = GameObject.Find("World").GetComponent<CWorldManager>();
+        Actor3D = transform.Find("Root3D").gameObject;
+        Actor2D = transform.Find("Root2D").gameObject;
+
+        bState3D = true;
+        bState2D = false;
+        OutViewRect = true;
+
+    }
+
+    void Update()
+    {
+        if (WorldManager)
+        {
+            if (WorldManager.CurrentWorldState == EWorldState.Changing)
+            {
+                if (BeforeState == EWorldState.View3D && !IsChanging)
+                {
+                    if (bState3D)//2D로 바꾸기
+                    {
+                        if (Actor2D.GetComponent<SpriteRenderer>())
+                            SetAllActorActive(true, false);
+
+                        BeforeState = EWorldState.View2D;
+                        bState2D = true;
+                        bState3D = false;
+
+                        if (OutViewRect)
+                            SetAllActorActive(false, false);
+                        else
+                            Actor2D.SetActive(true);
+                    }
+                }
+                else if (BeforeState == EWorldState.View2D && !IsChanging)
+                {
+                    if (bState2D)//3D로 바꾸기
+                    {
+                        BeforeState = EWorldState.View3D;
+                        Actor3D.SetActive(true);
+                        Actor2D.SetActive(false);
+                        bState3D = true;
+                        bState2D = false;
+                        OutViewRect = true;
+                    }
+                }
+
+                IsChanging = true;
+            }
+            else
+                IsChanging = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OutViewRect = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OutViewRect = true;
+        }
+    }
+
+    void SetAllActorActive(bool A, bool B)
+    {
+        Actor3D.SetActive(A);
+        Actor2D.SetActive(B);
+    }
+
+}
