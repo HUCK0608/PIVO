43b448f8814d21043400d69d31a921518ee5a988
김명준 <myungjun10@naver.com>
2020-01-17 01:03:48 +0900

    작은 눈더미 수정 / 스테이지5 레벨 수정 / 엔딩크레딧 1안 제작

---
+++ Design/DesignEndingCredit/Design_EndingCredit.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum EndingCreditType { None, First }
+
+public class Design_EndingCredit : MonoBehaviour
+{
+    private RawImage EndingLogo = null;
+    private Text TargetText = null;
+
+    [SerializeField]
+    private EndingCreditType CreditType = EndingCreditType.None;
+
+    float ShowLogoValue = 0;
+    float ShowTextValue = 0;
+    bool bShowTextCoroutine = false;
+
+    void Start()
+    {
+        if (CreditType == EndingCreditType.First)
+        {
+            EndingLogo = transform.Find("Canvas_1").Find("LogoImage").GetComponent<RawImage>();
+            TargetText = transform.Find("Canvas_1").Find("Text").GetComponent<Text>();
+
+            EndingLogo.color = new Color(1, 1, 1, 0);
+            TargetText.color = new Color(1, 1, 1, 0);
+
+            StartCoroutine(EndingCreditCoroutine());
+        }
+    }
+
+    IEnumerator EndingCreditCoroutine()
+    {
+
+        yield return new WaitForSeconds(1);
+
+        while (ShowLogoValue < 1)
+        {
+            ShowLogoValue += 0.01f;
+            EndingLogo.color = new Color(1, 1, 1, ShowLogoValue);
+            yield return new WaitForFixedUpdate();
+        }
+
+
+        float WaitTerm = 1f;
+
+        StartCoroutine(ShowTextCoroutine("GameDesign : KimMyungJun"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("GameDesign : KimHeonJu"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("Programming : KimSangHyuck"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("Animation : KimDoYeon"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("ConceptArt/Voxel : KimJuYoung"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("UI/Pixel : HanEunHye"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("Special Thanks : JungJongPil"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        StartCoroutine(ShowTextCoroutine("END"));
+        yield return new WaitUntil(() => !bShowTextCoroutine);
+        yield return new WaitForSeconds(WaitTerm);
+
+        while (ShowLogoValue > 0)
+        {
+            ShowLogoValue -= 0.01f;
+            EndingLogo.color = new Color(1, 1, 1, ShowLogoValue);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+
+
+    IEnumerator ShowTextCoroutine(string TText)
+    {
+        bShowTextCoroutine = true;
+        ShowTextValue = 0;
+        float ShowSpeed = 0.02f;
+        float HideSpeed = 0.02f;
+
+        while (ShowTextValue < 1)
+        {
+            ShowTextValue += ShowSpeed;
+            TargetText.color = new Color(1, 1, 1, ShowTextValue);
+            TargetText.text = TText;
+            yield return new WaitForFixedUpdate();
+        }
+
+        ShowTextValue = 1;
+
+        while (ShowTextValue > 0)
+        {
+            ShowTextValue -= HideSpeed;
+            TargetText.color = new Color(1, 1, 1, ShowTextValue);
+            yield return new WaitForFixedUpdate();
+        }
+        bShowTextCoroutine = false;
+    }
+    
+}
