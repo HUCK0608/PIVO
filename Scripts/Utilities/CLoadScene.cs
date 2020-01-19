4f08a995635483e61e7574c3310c04c9cdd25e90
김상혁 <fear2002@naver.com>
2020-01-20 03:45:44 +0900

    1. 로딩 씬 추가

---
+++ Scripts/Utilities/CLoadScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CLoadScene : MonoBehaviour
+{
+    [SerializeField]
+    private string _sceneName = string.Empty;
+    [SerializeField]
+    private float _delayTime = 0f;
+
+    private float _currentTime = 0f;
+
+    private bool _isLoading = false;
+
+    void Update()
+    {
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime >= _delayTime)
+            LoadScene();
+    }
+
+    private void LoadScene()
+    {
+        if (_isLoading)
+            return;
+
+        SceneManager.LoadScene(_sceneName);
+        _isLoading = true;
+    }
+}
