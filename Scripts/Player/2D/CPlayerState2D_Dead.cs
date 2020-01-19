a0840b6bfaa668a2205f37c776bc36d32dcad519
김상혁 <fear2002@naver.com>
2020-01-19 23:38:01 +0900

    1. 2D 죽음상태일 때 UI가 나오지 않는 문제 수정

--- Scripts/Player/2D/CPlayerState2D_Dead.cs
+++ Scripts/Player/2D/CPlayerState2D_Dead.cs
@@ -1,3 +1,19 @@
-﻿public class CPlayerState2D_Dead : CPlayerState2D
+﻿using UnityEngine;
+
+public class CPlayerState2D_Dead : CPlayerState2D
 {
+    public override void InitState()
+    {
+        base.InitState();
+
+        CUIManager.Instance.ActiveDeadUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+            CUIManager.Instance.DeadUIChangeSelectMenu();
+        else if (Input.GetKeyDown(CKeyManager.ViewChangeExecutionKey))
+            CUIManager.Instance.DeadUIExcutionSelectMenu();
+    }
 }
