2483e2b7420ceffc72fd3f5c479792c843afa68b
김상혁 <fear2002@naver.com>
2020-02-02 16:33:26 +0900

    1. 인게임에 UI에 옵션적용 2. New Game시 저장된 플레이어 프리팹 모두 제거 3. 빌드 에러나는 스크립트 주석처리

--- Design/DesignScript/DesignLevelTool/Design_FindTileError.cs
+++ Design/DesignScript/DesignLevelTool/Design_FindTileError.cs
@@ -5,157 +5,157 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Design_FindTileError : MonoBehaviour
 {
-    public bool bCheckOverlap = false;
-    public bool bCheckRot = false;
-    public bool bCheckPos = false;
-    public bool bRoundPos = false;
-    public bool bCheckBool = false;
+    //public bool bCheckOverlap = false;
+    //public bool bCheckRot = false;
+    //public bool bCheckPos = false;
+    //public bool bRoundPos = false;
+    //public bool bCheckBool = false;
 
-    void Update()
-    {
-        CheckTilePos();
-        CheckTileRot();
-        CheckOverlap();
-        SetRoudPos();
-        CheckBoolean();
-    }
-
-
-    void SetRoudPos()
-    {
-        if (bRoundPos)
-        {
-            int ChildNum = transform.childCount;
-
-            for (int i = 0; i < ChildNum; i++)
-            {
-                float X = transform.GetChild(i).transform.position.x;
-                float Y = transform.GetChild(i).transform.position.y;
-                float Z = transform.GetChild(i).transform.position.z;
-
-                X = Mathf.Round(X);
-                Y = Mathf.Round(Y);
-                Z = Mathf.Round(Z);
-
-                transform.GetChild(i).transform.position = new Vector3(X, Y, Z);
-            }
-            bRoundPos = false;
-            Debug.Log("Finish");
-        }
-    }
+    //void Update()
+    //{
+    //    CheckTilePos();
+    //    CheckTileRot();
+    //    CheckOverlap();
+    //    SetRoudPos();
+    //    CheckBoolean();
+    //}
 
 
-    void CheckOverlap()
-    {
-        if (bCheckOverlap)
-        {
-            int ChildNum = transform.childCount;
+    //void SetRoudPos()
+    //{
+    //    if (bRoundPos)
+    //    {
+    //        int ChildNum = transform.childCount;
 
-            for (int i = 0; i < ChildNum; i++)
-            {
-                for (int j = 0; j < ChildNum; j++)
-                {
-                    if (i != j && transform.GetChild(i).transform.position == transform.GetChild(j).transform.position)
-                    {
-                        Debug.Log(transform.GetChild(i).name + " : " + transform.GetChild(j).name);
-                    }
-                }
-            }
-            bCheckOverlap = false;
-            Debug.Log("Finish");
-        }
-    }
+    //        for (int i = 0; i < ChildNum; i++)
+    //        {
+    //            float X = transform.GetChild(i).transform.position.x;
+    //            float Y = transform.GetChild(i).transform.position.y;
+    //            float Z = transform.GetChild(i).transform.position.z;
 
-    void CheckTileRot()
-    {
-        if (bCheckRot)
-        {
-            int ChildNum = transform.childCount;
+    //            X = Mathf.Round(X);
+    //            Y = Mathf.Round(Y);
+    //            Z = Mathf.Round(Z);
 
-            for (int i = 0; i < ChildNum; i++)
-            {
-                bool IsError = false;
-                float X = transform.GetChild(i).transform.rotation.x;
-                float Y = transform.GetChild(i).transform.rotation.y;
-                float Z = transform.GetChild(i).transform.rotation.z;
-
-                if (X != 0)
-                    IsError = true;
-                else if (Y != 0)
-                    IsError = true;
-                else if (Z != 0)
-                    IsError = true;
-
-                if (IsError)
-                {
-                    Debug.Log(transform.GetChild(i).name);
-                }
-            }
-            bCheckRot = false;
-            Debug.Log("Finish");
-        }
-    }
+    //            transform.GetChild(i).transform.position = new Vector3(X, Y, Z);
+    //        }
+    //        bRoundPos = false;
+    //        Debug.Log("Finish");
+    //    }
+    //}
 
 
-    void CheckTilePos()
-    {
-        if (bCheckPos)
-        {
-            int ChildNum = transform.childCount;
+    //void CheckOverlap()
+    //{
+    //    if (bCheckOverlap)
+    //    {
+    //        int ChildNum = transform.childCount;
 
-            for (int i = 0; i < ChildNum; i++)
-            {
-                bool IsError = false;
-                float X = transform.GetChild(i).transform.position.x;
-                float Y = transform.GetChild(i).transform.position.y;
-                float Z = transform.GetChild(i).transform.position.z;
+    //        for (int i = 0; i < ChildNum; i++)
+    //        {
+    //            for (int j = 0; j < ChildNum; j++)
+    //            {
+    //                if (i != j && transform.GetChild(i).transform.position == transform.GetChild(j).transform.position)
+    //                {
+    //                    Debug.Log(transform.GetChild(i).name + " : " + transform.GetChild(j).name);
+    //                }
+    //            }
+    //        }
+    //        bCheckOverlap = false;
+    //        Debug.Log("Finish");
+    //    }
+    //}
 
-                if (X % 2 != 0)
-                    IsError = true;
-                else if (Y % 2 != 0)
-                    IsError = true;
-                else if (Z % 2 != 0)
-                    IsError = true;
+    //void CheckTileRot()
+    //{
+    //    if (bCheckRot)
+    //    {
+    //        int ChildNum = transform.childCount;
 
-                if (IsError)
-                {
-                    Debug.Log(transform.GetChild(i).name);
-                }
-            }
-            bCheckPos = false;
-            Debug.Log("Finish");
-        }
-    }
+    //        for (int i = 0; i < ChildNum; i++)
+    //        {
+    //            bool IsError = false;
+    //            float X = transform.GetChild(i).transform.rotation.x;
+    //            float Y = transform.GetChild(i).transform.rotation.y;
+    //            float Z = transform.GetChild(i).transform.rotation.z;
 
-    void CheckBoolean()
-    {
-        if(bCheckBool)
-        {
-            int ChildNum = transform.childCount;
+    //            if (X != 0)
+    //                IsError = true;
+    //            else if (Y != 0)
+    //                IsError = true;
+    //            else if (Z != 0)
+    //                IsError = true;
 
-            for (int i = 0; i < ChildNum; i++)
-            {
-                bool IsError = false;
-                List<bool> CheckBoolean = new List<bool>();
-                CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotPlus);
-                CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotMinus);
-                CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotZero);
+    //            if (IsError)
+    //            {
+    //                Debug.Log(transform.GetChild(i).name);
+    //            }
+    //        }
+    //        bCheckRot = false;
+    //        Debug.Log("Finish");
+    //    }
+    //}
+
+
+    //void CheckTilePos()
+    //{
+    //    if (bCheckPos)
+    //    {
+    //        int ChildNum = transform.childCount;
+
+    //        for (int i = 0; i < ChildNum; i++)
+    //        {
+    //            bool IsError = false;
+    //            float X = transform.GetChild(i).transform.position.x;
+    //            float Y = transform.GetChild(i).transform.position.y;
+    //            float Z = transform.GetChild(i).transform.position.z;
+
+    //            if (X % 2 != 0)
+    //                IsError = true;
+    //            else if (Y % 2 != 0)
+    //                IsError = true;
+    //            else if (Z % 2 != 0)
+    //                IsError = true;
+
+    //            if (IsError)
+    //            {
+    //                Debug.Log(transform.GetChild(i).name);
+    //            }
+    //        }
+    //        bCheckPos = false;
+    //        Debug.Log("Finish");
+    //    }
+    //}
+
+    //void CheckBoolean()
+    //{
+    //    if(bCheckBool)
+    //    {
+    //        int ChildNum = transform.childCount;
+
+    //        for (int i = 0; i < ChildNum; i++)
+    //        {
+    //            bool IsError = false;
+    //            List<bool> CheckBoolean = new List<bool>();
+    //            CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotPlus);
+    //            CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotMinus);
+    //            CheckBoolean.Add(transform.GetChild(i).GetComponent<Design_MaterialChange>().RotZero);
                 
 
-                foreach (var BoolValue in CheckBoolean)
-                {
-                    if (BoolValue)
-                        IsError = true;
-                }
+    //            foreach (var BoolValue in CheckBoolean)
+    //            {
+    //                if (BoolValue)
+    //                    IsError = true;
+    //            }
 
-                if (IsError)
-                {
-                    Debug.Log(transform.GetChild(i).name);
-                }
-            }
-            bCheckBool = false;
-            Debug.Log("Finish");
-        }
-    }
+    //            if (IsError)
+    //            {
+    //                Debug.Log(transform.GetChild(i).name);
+    //            }
+    //        }
+    //        bCheckBool = false;
+    //        Debug.Log("Finish");
+    //    }
+    //}
 
 }
--- Scripts/UI/CUIManager.cs
+++ Scripts/UI/CUIManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.Audio;
 
 public enum EPaintingType { Snow = 0, Temple };
 
@@ -34,10 +35,36 @@ public class CUIManager : MonoBehaviour
                 SetActiveRestart(false);
                 return;
             }
+            else if(_optionGroup.activeSelf)
+            {
+                CancelOption();
+                return;
+            }
 
             if (!_pauseGroup.activeSelf)
                 SetActivePause(true);
             else
+                SetActivePause(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.X))
+        {
+            if (_returnToTitleGroup.activeSelf)
+            {
+                SetActiveReturnToTitle(false);
+                return;
+            }
+            else if (_restartGroup.activeSelf)
+            {
+                SetActiveRestart(false);
+                return;
+            }
+            else if (_optionGroup.activeSelf)
+            {
+                CancelOption();
+                return;
+            }
+
+            if(_pauseGroup.activeSelf)
                 SetActivePause(false);
         }
 
@@ -52,6 +79,9 @@ public class CUIManager : MonoBehaviour
 
         if (_restartGroup.activeSelf)
             RestartLogic();
+
+        if (_optionGroup.activeSelf)
+            OptionLogic();
     }
 
     /// <summary>캔버스</summary>
@@ -591,7 +621,8 @@ public class CUIManager : MonoBehaviour
 
     private void ExcuteOption_Pause()
     {
-
+        SetActivePause(false, false);
+        SetActiveOption(true);
     }
 
     private void ExcuteExit_Pause()
@@ -802,6 +833,345 @@ public class CUIManager : MonoBehaviour
     {
         SetActiveRestart(false);
     }
+
+    #endregion
+
+    #region Option
+
+    /// <summary>옵션 메뉴 그룹</summary>
+    [SerializeField]
+    private GameObject _optionGroup = null;
+
+    /// <summary>선택 옵션 메뉴 백 이미지</summary>
+    [SerializeField]
+    private Transform _selectOptionMenuBG = null;
+    /// <summary>현재 옵션 선택 메뉴</summary>
+    private int _currentSelectMenu_OptionMenu = 0;
+
+    private bool _isOptionInitialize = false;
+
+    /// <summary>옵션 설정 저장</summary>
+    private void SaveOptionData()
+    {
+        EXmlDocumentNames documentName = EXmlDocumentNames.Setting;
+        string nodePath = documentName.ToString("G");
+        string[] elementsName = new string[] { "WindowMode", "Resolution", "BGM", "SFX" };
+        string[] datas = new string[] { ((int)_currentWindowMode).ToString(),
+                                        _currentResolution.ToString(),
+                                        _currentBGMNormalizedValue.ToString(),
+                                        _currentSFXNormalizedValue.ToString() };
+
+        CDataManager.WritingDatas(EXmlDocumentNames.Setting, nodePath, elementsName, datas, true);
+        CDataManager.SaveCurrentXmlDocument(true);
+    }
+
+    /// <summary>옵션 설정 로드</summary>
+    private void LoadOptionData()
+    {
+        EXmlDocumentNames documentName = EXmlDocumentNames.Setting;
+        string nodePath = documentName.ToString("G");
+        string[] elementsName = new string[] { "WindowMode", "Resolution", "BGM", "SFX" };
+
+        string[] datas = CDataManager.ReadDatas(documentName, nodePath, elementsName);
+
+        if (datas == null)
+            return;
+
+        _currentWindowMode = (EWindowMode)int.Parse(datas[0]);
+        _selectWindowMode = _currentWindowMode;
+        _currentResolution = int.Parse(datas[1]);
+        _selectResolution = _currentResolution;
+        _currentBGMNormalizedValue = float.Parse(datas[2]);
+        _selectBGMNormalizedValue = _currentBGMNormalizedValue;
+        _currentSFXNormalizedValue = float.Parse(datas[3]);
+        _selectSFXNormalizedValue = _currentSFXNormalizedValue;
+
+        UpdateOptionUI();
+
+        ApplyOption();
+    }
+
+    /// <summary>옵션 메뉴 활성화</summary>
+    private void SetActiveOption(bool active)
+    {
+        _isOptionInitialize = false;
+        _optionGroup.SetActive(active);
+
+        if (active)
+            PlayerCanOperationFalse();
+        else
+            Invoke("PlayerCanOperationTrue", Time.deltaTime);
+
+        CCameraController.Instance.SetActivateBlur(active);
+
+        if (active)
+            InitOption();
+    }
+
+    private void InitOption()
+    {
+        // 선택 메뉴 초기화(맨 위)
+        SetSelectOptionMenuBGPoint(0);
+
+        LoadOptionData();
+    }
+
+    /// <summary>선택 옵션 메뉴 BG 위치 설정</summary>
+    public void SetSelectOptionMenuBGPoint(int selectMenuValue)
+    {
+        if (_currentSelectMenu_OptionMenu.Equals(selectMenuValue))
+            return;
+
+        _currentSelectMenu_OptionMenu = selectMenuValue;
+
+        Vector3 temp = Vector3.up * 70f;
+        _selectOptionMenuBG.localPosition = -temp * _currentSelectMenu_OptionMenu + temp + Vector3.right * -65f;
+
+        PlayPointerUpAudio();
+    }
+
+    /// <summary>옵션 업데이트</summary>
+    private void UpdateOptionUI()
+    {
+        UpdateOptionUI_WindowMode();
+        UpdateOptionUI_Resolution();
+        UpdateOptionUI_BGM();
+        UpdateOptionUI_SFX();
+    }
+
+    private void ChangeOption(float addValue)
+    {
+        switch (_currentSelectMenu_OptionMenu)
+        {
+            case 0:
+                ChangeWindowMode((int)addValue);
+                break;
+            case 1:
+                ChangeResolution((int)addValue);
+                break;
+            case 2:
+                ChangeBGMVolume(addValue);
+                break;
+            case 3:
+                ChangeSFXVolume(addValue);
+                break;
+        }
+    }
+
+    /// <summary>옵션 로직</summary>
+    private void OptionLogic()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Return))
+        {
+            ApplyOption();
+            _isOptionInitialize = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SetSelectOptionMenuBGPoint(Mathf.Clamp(_currentSelectMenu_OptionMenu - 1, 0, 3));
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetSelectOptionMenuBGPoint(Mathf.Clamp(_currentSelectMenu_OptionMenu + 1, 0, 3));
+        }
+        else if (_currentSelectMenu_OptionMenu == 0 || _currentSelectMenu_OptionMenu == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                ChangeOption(-1f);
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                ChangeOption(1f);
+        }
+        else if (_currentSelectMenu_OptionMenu == 2 || _currentSelectMenu_OptionMenu == 3)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                ChangeOption(-0.01f);
+            else if (Input.GetKey(KeyCode.RightArrow))
+                ChangeOption(0.01f);
+        }
+    }
+
+    /// <summary>옵션 적용</summary>
+    public void ApplyOption()
+    {
+        ApplyOption_WindowModeWithResolution();
+        ApplyOption_Sound();
+
+        if (_isOptionInitialize)
+        {
+            SaveOptionData();
+            PlayPointerEnterAudio();
+        }
+    }
+
+    /// <summary>윈도우 모드 및 해상도 변경</summary>
+    private void ApplyOption_WindowModeWithResolution()
+    {
+        _currentWindowMode = _selectWindowMode;
+        _currentResolution = _selectResolution;
+
+        if (_currentWindowMode.Equals(EWindowMode.FullScreen))
+            Screen.SetResolution(_resolution1[_currentResolution], _resolution2[_currentResolution], FullScreenMode.ExclusiveFullScreen);
+        else if (_currentWindowMode.Equals(EWindowMode.FullScreenWindow))
+            Screen.SetResolution(_resolution1[_currentResolution], _resolution2[_currentResolution], FullScreenMode.FullScreenWindow);
+        else
+            Screen.SetResolution(_resolution1[_currentResolution], _resolution2[_currentResolution], FullScreenMode.Windowed);
+    }
+
+    /// <summary>사운드 적용</summary>
+    private void ApplyOption_Sound()
+    {
+        _currentBGMNormalizedValue = _selectBGMNormalizedValue;
+        _currentSFXNormalizedValue = _selectSFXNormalizedValue;
+
+        string volume = "Volume";
+        _BGMAudioMixer.SetFloat(volume, 80f * _currentBGMNormalizedValue - 80f);
+        _SFXAudioMixer.SetFloat(volume, 80f * _currentSFXNormalizedValue - 80f);
+    }
+
+    /// <summary>옵션 적용 취소</summary>
+    public void CancelOption()
+    {
+        CancelWindowMode();
+        CancelResolution();
+        CancelSound();
+
+        UpdateOptionUI();
+
+        SetActiveOption(false);
+    }
+
+    /// <summary>윈도우 모드 설정 취소</summary>
+    private void CancelWindowMode() { _selectWindowMode = _currentWindowMode; }
+
+    /// <summary>해상도 설정 취소</summary>
+    private void CancelResolution() { _selectResolution = _currentResolution; }
+
+    /// <summary>사운드 설정 취소</summary>
+    private void CancelSound()
+    {
+        _selectBGMNormalizedValue = _currentBGMNormalizedValue;
+        _selectSFXNormalizedValue = _currentSFXNormalizedValue;
+    }
+
+
+    ///////////////////////////////////////// Window Mode
+    /// <summary>윈도우 모드 Enum</summary>
+    private enum EWindowMode { FullScreen = 0, FullScreenWindow = 1, Windowed };
+    /// <summary>현재 윈도우 모드</summary>
+    private EWindowMode _currentWindowMode = EWindowMode.FullScreen;
+    /// <summary>선택 윈도우 모드</summary>
+    private EWindowMode _selectWindowMode = EWindowMode.FullScreen;
+
+    /// <summary>윈도우 모드 값 텍스트</summary>
+    [SerializeField]
+    private Text _windowModeValueText = null;
+
+    /// <summary>
+    /// 윈도우 모드 설정
+    /// </summary>
+    /// <param name="addValue">추가 값</param>
+    public void ChangeWindowMode(int addValue)
+    {
+        _selectWindowMode += addValue;
+
+        if (((int)_selectWindowMode).Equals(-1))
+            _selectWindowMode = (EWindowMode)(System.Enum.GetValues(typeof(EWindowMode)).Length - 1);
+        else if (((int)_selectWindowMode).Equals(System.Enum.GetValues(typeof(EWindowMode)).Length))
+            _selectWindowMode = 0;
+
+        UpdateOptionUI_WindowMode();
+
+        PlayPointerUpAudio();
+    }
+
+    /// <summary>윈도우 모드 UI 업데이트</summary>
+    private void UpdateOptionUI_WindowMode()
+    {
+        UpdateWindowModeText();
+    }
+
+    /// <summary>윈도우 모드 텍스트 업데이트</summary>
+    private void UpdateWindowModeText() { _windowModeValueText.text = _selectWindowMode.ToString("G"); }
+
+
+    ///////////////////////////////////////// Resoultion
+    /// <summary>현재 해상도</summary>
+    private int _currentResolution = 0;
+    /// <summary>선택 해상도</summary>
+    private int _selectResolution = 0;
+
+    /// <summary>해상도</summary>
+    [SerializeField]
+    private List<int> _resolution1 = null, _resolution2 = null;
+
+    /// <summary>해상도 값 텍스트</summary>
+    [SerializeField]
+    private Text _resolutionValueText = null;
+
+    /// <summary>
+    /// 해상도 설정
+    /// </summary>
+    /// <param name="addValue">추가 값</param>
+    public void ChangeResolution(int addValue)
+    {
+        _selectResolution += addValue;
+
+        if (_selectResolution.Equals(-1))
+            _selectResolution = _resolution1.Count - 1;
+        else if (_selectResolution.Equals(_resolution1.Count))
+            _selectResolution = 0;
+
+        UpdateOptionUI_Resolution();
+
+        PlayPointerUpAudio();
+    }
+
+    /// <summary>해상도 UI 업데이트</summary>
+    private void UpdateOptionUI_Resolution()
+    {
+        UpdateResolutionText();
+    }
+
+    /// <summary>해상도 텍스트 업데이트</summary>
+    private void UpdateResolutionText() { _resolutionValueText.text = _resolution1[_selectResolution].ToString() + " * " + _resolution2[_selectResolution].ToString(); }
+
+
+    ///////////////////////////////////////// Sound
+    /// <summary>BGM 오디오 믹서</summary>
+    [SerializeField]
+    private AudioMixer _BGMAudioMixer = null;
+    /// <summary>BGM 스크롤</summary>
+    [SerializeField]
+    private CScroll _BGMScroll = null;
+
+    /// <summary>SFX 오디오 믹서</summary>
+    [SerializeField]
+    private AudioMixer _SFXAudioMixer = null;
+    /// <summary>SFX 스크롤</summary>
+    [SerializeField]
+    private CScroll _SFXScroll = null;
+
+    /// <summary>현재 BGM 노말 값</summary>
+    private float _currentBGMNormalizedValue = 1f;
+    /// <summary>선택 BGM 노말 값</summary>
+    private float _selectBGMNormalizedValue = 1f;
+
+    /// <summary>현재 SFX 노말 값</summary>
+    private float _currentSFXNormalizedValue = 1f;
+    /// <summary>선택 SFX 노말 값</summary>
+    private float _selectSFXNormalizedValue = 1f;
+
+    /// <summary>BGM UI 업데이트</summary>
+    private void UpdateOptionUI_BGM() { _BGMScroll.SetScroll(_currentBGMNormalizedValue); }
+    /// <summary>SFX UI 업데이트</summary>
+    private void UpdateOptionUI_SFX() { _SFXScroll.SetScroll(_currentSFXNormalizedValue); }
+
+    /// <summary>BGM 볼륨 설정</summary>
+    public void ChangeBGMVolume() { _selectBGMNormalizedValue = _BGMScroll.NormalizedValue; }
+    /// <summary>SFX 볼륨 설정</summary>
+    public void ChangeSFXVolume() { _selectSFXNormalizedValue = _SFXScroll.NormalizedValue; }
+    public void ChangeBGMVolume(float addValue) { _selectBGMNormalizedValue = Mathf.Clamp(_selectBGMNormalizedValue + addValue, 0f, 1f); _BGMScroll.SetScroll(_selectBGMNormalizedValue); }
+    public void ChangeSFXVolume(float addValue) { _selectSFXNormalizedValue = Mathf.Clamp(_selectSFXNormalizedValue + addValue, 0f, 1f); _SFXScroll.SetScroll(_selectSFXNormalizedValue); }
 
     #endregion
 
