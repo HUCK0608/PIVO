b4993e079ea40a3f5b5be6f244218bc12ab545f0
김상혁 <fear2002@naver.com>
2020-03-26 22:49:43 +0900

    [버그] 숲숲이가 코기를 들어올릴 때 정상적인 위치로 들어올리도록 적용

--- Scripts/Character/Soop/2D/CSoopState2D_PutInit.cs
+++ Scripts/Character/Soop/2D/CSoopState2D_PutInit.cs
@@ -1,14 +1,30 @@
-﻿public class CSoopState2D_PutInit : CSoopState2D
+﻿using UnityEngine;
+
+public class CSoopState2D_PutInit : CSoopState2D
 {
+    [SerializeField]
+    private Transform _putInitPoint = null;
+
+    private float _increaseValue = 1.25f;
+    private float _addTime = 0f;
+
     public override void InitState()
     {
         base.InitState();
 
         CPlayerManager.Instance.Controller2D.ChangeState(EPlayerState2D.PutInit);
+
+        _addTime = 0f;
     }
 
     private void Update()
     {
+        _addTime += Time.deltaTime;
+        Transform player2DTransform = CPlayerManager.Instance.Controller2D.transform;
+        Vector3 destination = _putInitPoint.position;
+        destination.z = player2DTransform.position.z;
+        player2DTransform.position = Vector3.MoveTowards(player2DTransform.transform.position, destination, Mathf.Clamp(_increaseValue * _addTime, 0f, 1f));
+
         if (CPlayerManager.Instance.Stat.IsPut)
             Controller2D.ChangeState(ESoopState.PutMove);
     }
--- Scripts/Character/Soop/3D/CSoopState3D_PutInit.cs
+++ Scripts/Character/Soop/3D/CSoopState3D_PutInit.cs
@@ -2,15 +2,27 @@
 
 public class CSoopState3D_PutInit : CSoopState3D
 {
+    [SerializeField]
+    private Transform _putInitPoint = null;
+
+    private float _increaseValue = 0.4f;
+    private float _addTime = 0f;
+
     public override void InitState()
     {
         base.InitState();
 
         CPlayerManager.Instance.Controller3D.ChangeState(EPlayerState3D.PutInit);
+
+        _addTime = 0f;
     }
 
     private void Update()
     {
+        _addTime += Time.deltaTime;
+        Transform player3DTransform = CPlayerManager.Instance.Controller3D.transform;
+        player3DTransform.position = Vector3.MoveTowards(player3DTransform.transform.position, _putInitPoint.position, Mathf.Clamp(_increaseValue * _addTime, 0f, 1f));
+
         if (CPlayerManager.Instance.Stat.IsPut)
             Controller3D.ChangeState(ESoopState.PutMove);
     }
