aa45530fe5f6158b675ef1c552b487541a5b1e15
김상혁 <fear2002@naver.com>
2020-04-12 23:25:36 +0900

    [추가] 메인씬 추가 및 씬 로딩 순서 변경 메인->인게임 진입

---
+++ Scripts/Main/CMainManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CMainManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _dontDestroyManagerList = null;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(_dontDestroyManagerList);
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene("GrassStage_Stage1");
+    }
+}
